cf4b450013367f6c275feb2cf9ce86b2ddbb39ea
Rubin Jean <AYDYV0@ad.insee.intra>
2024-04-08 17:43:33 +0200

    Introduce Goodman-Bacon pairwise plots

--- R/logic_goodman_bacon_decomp.R
+++ R/logic_goodman_bacon_decomp.R
@@ -7,43 +7,9 @@ did_estimate <- function(data, groups, event) {
   diff_post <- mean(y_post_t - y_post_u)
   diff_pre <- mean(y_pre_t - y_pre_u)
 
-  ifelse(is.na(diff_post), 0, diff_post) - ifelse(is.na(diff_pre), 0, diff_pre)
-}
+  if (is.na(diff_post) || is.na(diff_pre)) return(0)
 
-goodman_bacon_decomp <- function(timeline, treated_groups, control_group) {
-  nb_times <- length(timeline)
-  n_tot <- treated_groups[[1]]$size +
-    treated_groups[[2]]$size +
-    control_group$size
-
-  n_u <- control_group$size / n_tot
-  n_1 <- treated_groups[[1]]$size / n_tot
-  n_2 <- treated_groups[[2]]$size / n_tot
-  n_1u <- n_1 / (n_1 + n_u)
-  n_2u <- n_2 / (n_2 + n_u)
-  n_12 <- n_1 / (n_1 + n_2)
-  d_1 <- min_max((nb_times - treated_groups[[1]]$event) / nb_times, 0, 1)
-  d_2 <- min_max((nb_times - treated_groups[[2]]$event) / nb_times, 0, 1)
-  v_1u <- n_1u * (1 - n_1u) * d_1 * (1 - d_1)
-  s_1u <- (n_1 + n_u)^2 * v_1u
-  v_2u <- n_2u * (1 - n_2u) * d_2 * (1 - d_2)
-  s_2u <- (n_2 + n_u)^2 * v_2u
-  v_12_1 <- n_12 * (1 - n_12) * (d_1 - d_2) * (1 - d_1) / (1 - d_2)^2
-  s_12_1 <- ((n_1 + n_2) * (1 - d_2))^2 * v_12_1
-  v_12_2 <- n_12 * (1 - n_12) * d_2 * (d_1 - d_2) / d_1^2
-  s_12_2 <-  ((n_1 + n_2) * d_1)^2 * v_12_2
-  s_tot <- s_1u + s_2u + s_12_1 + s_12_2
-  s_1u_n <- s_1u / s_tot
-  s_2u_n <- s_2u / s_tot
-  s_12_1_n <- s_12_1 / s_tot
-  s_12_2_n <- s_12_2 / s_tot
-
-  # n_u -> n_2
-  # n_1u -> n_12
-  # 0 -> d_2
-  # v_1u -> v_12_1
-  # s_1u -> s_12_1
-  # d_2 -> 1 - d_1, d_1 -> 1 - d_2
+  diff_post - diff_pre
 }
 
 goodman_bacon_coef <- function(nb_times, group_1, group_2) {
@@ -70,63 +36,24 @@ goodman_bacon_coef <- function(nb_times, group_1, group_2) {
   )
 }
 
+did_estimates <- function(data_event, group_1, group_2) {
+  # Formula assumes that group_1$event < group_2$event
+  comparison <- if (group_1$event < group_2$event) `<` else `>=`
 
-remainder <- function() {
-beta_1u <- reactive({
-  did_estimate(
-    data_event(),
-    list(treated = "Treated 1", control = "Never treated"),
-    treated_groups[[1]]$event()
+  beta_12_1 <- did_estimate(
+    data_event |> filter(comparison(t, group_2$event)),
+    list(treated = group_1$name, control = group_2$name),
+    group_1$event
   )
-})
 
-beta_2u <- reactive({
-  did_estimate(
-    data_event(),
-    list(treated = "Treated 2", control = "Never treated"),
-    treated_groups[[2]]$event()
+  beta_12_2 <- did_estimate(
+    data_event |> filter(!comparison(t, group_1$event)),
+    list(treated = group_2$name, control = group_1$name),
+    group_2$event
   )
-})
 
-beta_12_1 <- reactive({
-  did_estimate(
-    data_event() |> filter(t < treated_groups[[2]]$event()),
-    list(treated = "Treated 1", control = "Treated 2"),
-    treated_groups[[1]]$event()
+  list(
+    beta_12_1 = beta_12_1,
+    beta_12_2 = beta_12_2
   )
-})
-
-beta_12_2 <- reactive({
-  did_estimate(
-    data_event() |> filter(t >= treated_groups[[1]]$event()),
-    list(treated = "Treated 2", control = "Treated 1"),
-    treated_groups[[2]]$event()
-  )
-})
-
-pp <- function(x, ...) formatC(x, digits = 2, ...)
-output$analytic_decomposition <- renderUI({
-  withMathJax(HTML(
-    paste0("$$\\overline{D}_1 = ", pp(d_1()), "\\quad ",
-      "\\overline{D}_2 = ", pp(d_2()), "$$"),
-    paste0("$$s_{1U} = ", pp(s_1u_n()), "\\quad ",
-      "s_{2U} = ", pp(s_2u_n()), "$$"),
-    paste0("$$s_{12}^1 = ", pp(s_12_1_n()), "\\quad ",
-      "s_{12}^2 = ", pp(s_12_2_n()), "$$"),
-    "$$s_{1U}\\beta_{1U} + s_{2U}\\beta_{2U} + s_{12}^1\\beta_{12}^1 + s_{12}^2\\beta_{12}^2 = \\beta$$",
-    paste0(
-      "$$", pp(s_1u_n()), "\\times ", pp(beta_1u()),
-      " + ", pp(s_2u_n()), "\\times ", pp(beta_2u()),
-      " + ", pp(s_12_1_n()), "\\times ", pp(beta_12_1()),
-      " + ", pp(s_12_2_n()), "\\times ", pp(beta_12_2()),
-      " = ", pp(
-        s_1u_n() * beta_1u() +
-        s_12_1_n() * beta_12_1() +
-        s_2u_n() * beta_2u() +
-        s_12_2_n() * beta_12_2()
-      ),
-      "$$"
-    )
-  ))
-})
 }
--- R/mod_goodman_bacon_decomp.R
+++ R/mod_goodman_bacon_decomp.R
@@ -16,8 +16,14 @@ goodmanBaconDecomp_UI <- function(id) {
           )
         ),
         mainPanel(
-          plotOutput(ns("goodman_bacon_pairwise_plot")),
-          uiOutput(ns("analytic_decomposition"))
+          column(6,
+            plotOutput(ns("pairwise_plot_1")),
+            uiOutput(ns("analytic_decomposition_1"))
+          ),
+          column(6,
+            plotOutput(ns("pairwise_plot_2")),
+            uiOutput(ns("analytic_decomposition_2"))
+          )
         )
       )
     ),
@@ -27,6 +33,7 @@ goodmanBaconDecomp_UI <- function(id) {
 
 goodmanBaconDecomp_Server <- function(id, params_group_flat, data_event) {
   moduleServer(id, function(input, output, session) {
+    timeline <- session$userData$timeline
     groups_name <- reactive({
       data_event() |>
         pull(group) |>
@@ -44,6 +51,11 @@ goodmanBaconDecomp_Server <- function(id, params_group_flat, data_event) {
       updateSelectInput(session, "select_group_2", choices = second_groups_name())
     })
 
+    group_1 <- reactive(unpp_col(input$select_group_1))
+    group_2 <- reactive(unpp_col(input$select_group_2))
+    params_group_1 <- reactive(params_group_flat()[[group_1()]])
+    params_group_2 <- reactive(params_group_flat()[[group_2()]])
+
 
     goodman_bacon_coefs <- reactive({
       if (is.null(input$select_group_1) || is.null(input$select_group_2)) {
@@ -51,37 +63,73 @@ goodmanBaconDecomp_Server <- function(id, params_group_flat, data_event) {
       }
 
       goodman_bacon_coef(
-        11,
-        params_group_flat()[[unpp_col(input$select_group_1)]],
-        params_group_flat()[[unpp_col(input$select_group_2)]]
+        length(timeline),
+        params_group_1(),
+        params_group_2()
       )
     })
 
-    pp <- function(x, ...) formatC(x, digits = 2, ...)
-    output$analytic_decomposition <- renderUI({
-      withMathJax(HTML(goodman_bacon_coefs()[[1]]
+    goodman_bacon_did_estimates <- reactive({
+      if (is.null(input$select_group_1) || is.null(input$select_group_2)) {
+        return()
+      }
 
-        # paste0("$$\\overline{D}_1 = ", pp(d_1()), "\\quad ",
-        #        "\\overline{D}_2 = ", pp(d_2()), "$$"),
-        # paste0("$$s_{1U} = ", pp(s_1u_n()), "\\quad ",
-        #        "s_{2U} = ", pp(s_2u_n()), "$$"),
-        # paste0("$$s_{12}^1 = ", pp(s_12_1_n()), "\\quad ",
-        #        "s_{12}^2 = ", pp(s_12_2_n()), "$$"),
-        # "$$s_{1U}\\beta_{1U} + s_{2U}\\beta_{2U} + s_{12}^1\\beta_{12}^1 + s_{12}^2\\beta_{12}^2 = \\beta$$",
-        # paste0(
-        #   "$$", pp(s_1u_n()), "\\times ", pp(beta_1u()),
-        #   " + ", pp(s_2u_n()), "\\times ", pp(beta_2u()),
-        #   " + ", pp(s_12_1_n()), "\\times ", pp(beta_12_1()),
-        #   " + ", pp(s_12_2_n()), "\\times ", pp(beta_12_2()),
-        #   " = ", pp(
-        #     s_1u_n() * beta_1u() +
-        #       s_12_1_n() * beta_12_1() +
-        #       s_2u_n() * beta_2u() +
-        #       s_12_2_n() * beta_12_2()
-        #   ),
-        #   "$$"
-        # )
-      ))
+      did_estimates(
+        data_event(),
+        append(params_group_1(), list(name = group_1())),
+        append(params_group_2(), list(name = group_2()))
+      )
     })
+
+    comparison <- reactive({
+      if (params_group_1()$event < params_group_2()$event) `<` else `>=`
+    })
+
+    output$pairwise_plot_1 <- renderPlot({
+      data_event() |>
+        filter(
+          group %in% c(group_1(), group_2()),
+          comparison()(t, params_group_2()$event)
+        ) |>
+        pp_table() |>
+        plot_data(
+          c(params_group_1()$event, params_group_2()$event),
+          timeline
+        )
+    })
+
+    output$pairwise_plot_2 <- renderPlot({
+      data_event() |>
+        filter(
+          group %in% c(group_1(), group_2()),
+          !comparison()(t, params_group_1()$event)
+        ) |>
+        pp_table() |>
+        plot_data(
+          c(params_group_1()$event, params_group_2()$event),
+          timeline
+        )
+    })
+
+    pp <- function(x, ...) formatC(x, digits = 2, ...)
+
+    output$analytic_decomposition_1 <- renderUI({
+      withMathJax(HTML(paste0(
+        "$$",
+        "s_{12}^1 = ", pp(goodman_bacon_coefs()$s_12_1), "\\quad",
+        "\\beta_{12}^1 = ", pp(goodman_bacon_did_estimates()$beta_12_1),
+        "$$"
+      )))
+    })
+
+    output$analytic_decomposition_2 <- renderUI({
+      withMathJax(HTML(paste0(
+        "$$",
+        "s_{12}^2 = ", pp(goodman_bacon_coefs()$s_12_2), "\\quad",
+        "\\beta_{12}^2 = ", pp(goodman_bacon_did_estimates()$beta_12_2),
+        "$$"
+      )))
+    })
+
   })
 }
