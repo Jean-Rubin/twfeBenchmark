7da43df2159ae8c3338ccd6d30846a00511d3832
Rubin Jean <AYDYV0@ad.insee.intra>
2024-04-12 12:25:35 +0200

    Add new documentation

--- R/logic_utils.R
+++ R/logic_utils.R
@@ -4,7 +4,6 @@
 #'
 #' @return A list of list, where each sublist contains a `name` attribute
 #'   corresponding to its name.
-#' @export
 #'
 #' @examples
 #' insert_name(list(a = list(aa = 1, ab = 2), b = list()))
